70e1d486d84ac9e4de018ee06b87af5696272b5e
veera_1505 <veerasaraha@gmail.com>
2021-08-07 10:53:35 +0530

    react setup and add topbar

---
+++ client/src/components/topbar/Topbar.js
@@ -0,0 +1,46 @@
+import './Topbar.css'
+import { Search, Person, Chat, Notifications } from '@material-ui/icons'
+
+const Topbar = () => {
+  return (
+    <div className='topbarcontainer'>
+      <div className='topbarleft'>
+        <span className='logo'>social - connect</span>
+      </div>
+      <div className='topbarcenter'>
+        <div className='searchbar'>
+          <Search className='searchicon' />
+          <input
+            type='text'
+            placeholder='Searcg for frined, post or video'
+            className='searchinput'
+          />
+        </div>
+      </div>
+      <div className='topbarright'>
+        <div className='topbarlinks'>
+          <span className='topbarlink'>Homepage</span>
+          <span className='topbarlink'>Timeline</span>
+        </div>
+        <div className='topbaricons'>
+          <div className='topbariconitem'>
+            <Person />
+            <span className='topbariconbadge'>1</span>
+          </div>
+          <div className='topbariconitem'>
+            <Chat />
+            <span className='topbariconbadge'>2</span>
+          </div>
+          <div className='topbariconitem'>
+            <Notifications />
+            <span className='topbariconbadge'>1</span>
+          </div>
+        </div>
+
+        <img src='/assest/person/1.jpeg' alt='' className='topbarImg' />
+      </div>
+    </div>
+  )
+}
+
+export default Topbar
---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,12 @@
+import './Home.css'
+import Topbar from '../../components/topbar/Topbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  )
+}
+
+export default Home

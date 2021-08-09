5eb10096aac650043dabfc6bea3707247b3ac36f
veera_1505 <veerasaraha@gmail.com>
2021-08-09 22:53:14 +0530

    add create post in client

--- api/index.js
+++ api/index.js
@@ -5,26 +5,45 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-dotenv.config()
+const multer = require('multer')
 const DBConnection = require('./DB')
 const userRoute = require('./routes/users')
 const authRoute = require('./routes/auth')
 const postsRoute = require('./routes/posts')
+const path = require('path')
+
+dotenv.config()
 
 // DB Connection
 DBConnection()
 
 // Middleware
-// const options = {
-//   origin: 'http://127.0.0.1:3000',
-//   credentials: true,
-// }
+app.use('/images', express.static(path.join(__dirname, 'public/images')))
 app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet())
 app.use(morgan('common'))
+
+//Multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/images/')
+  },
+  filename: (req, file, cb) => {
+    console.log(req.body)
+    cb(null, req.body.name)
+  },
+})
+const upload = multer({ storage })
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  try {
+    return res.status(200).json('file uploaded successfully...')
+  } catch (error) {
+    console.log(error)
+  }
+})
 
 // Routes
 app.use('/api/users', userRoute)
